3b8a12674059e95695afd9d4444eb6a636cfa56e
Timothy-Dement <tmdement@ncsu.edu>
2018-02-12 15:41:07 -0500

    Fix external IP

--- main.js
+++ main.js
@@ -2,10 +2,15 @@ var express = require('express');
 var app = express();
 var server = app.listen(3000);
 var io = require('socket.io').listen(server);
+const IP = require('external-ip')();
 
-app.get('/', function(req, res)
+IP((err, ip) =>
 {
-    res.send('<h1 style="padding:25px; font-family:sans-serif">Hello, World!</h1>');
-});
+    console.log(`\nExample app listening at http://${ip}:3000\n`);
 
-console.log('\nExample application listening on port 3000\n');
+    app.get('/', (req, res) =>
+    {
+        var style = 'style="padding:25px;font-family:sans-serif"';
+        res.send(`<h1 ${style}>Hello, World!<br/><br/>[ ${ip} ]</h1>`);
+    });
+});
