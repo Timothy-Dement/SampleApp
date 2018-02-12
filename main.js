0c3677993ec9bbb8cd032c7765e28a8ff1fda4ee
Timothy-Dement <tmdement@ncsu.edu>
2018-02-12 15:47:57 -0500

    Check syntax

--- main.js
+++ main.js
@@ -2,15 +2,21 @@ var express = require('express');
 var app = express();
 var server = app.listen(3000);
 var io = require('socket.io').listen(server);
-const IP = require('external-ip')();
+// const IP = require('external-ip')();
 
-IP((err, ip) =>
+app.get('/', (req, res) =>
 {
-    console.log(`\nExample app listening at http://${ip}:3000\n`);
-
-    app.get('/', (req, res) =>
-    {
-        var style = 'style="padding:25px;font-family:sans-serif"';
-        res.send(`<h1 ${style}>Hello, World!<br/><br/>[ ${ip} ]</h1>`);
-    });
+    var style = 'style="padding:25px; font-family:sans-serif"';
+    res.send(`<h1 ${style}>Hello, World!</h1>`)
 });
+
+// IP((err, ip) =>
+// {
+//     console.log(`\nExample app listening at http://${ip}:3000\n`);
+
+//     app.get('/', (req, res) =>
+//     {
+//         var style = 'style="padding:25px;font-family:sans-serif"';
+//         res.send(`<h1 ${style}>Hello, World!<br/><br/>[ ${ip} ]</h1>`);
+//     });
+// });
