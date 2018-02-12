15d7ddc55c6f62bf27e319aee43736e005b2d175
Timothy-Dement <tmdement@ncsu.edu>
2018-02-12 15:55:12 -0500

    Add console log message

--- main.js
+++ main.js
@@ -7,3 +7,5 @@ app.get('/', (req, res) =>
     var style = 'style="padding:25px; font-family:sans-serif"';
     res.send(`<h1 ${style}>Hello, World!</h1>`)
 });
+
+console.log("\nExample app listening on port 3000...\n")
